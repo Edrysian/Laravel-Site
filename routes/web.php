f1650b7e6c1f91e488d325690e15fd83cb62bf6b
UP93769 <up938769@myport.ac.uk>
2024-09-19 08:36:50 +0100

    Adding database and about us page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/about', [PageController::class, 'about'])->name('about');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
