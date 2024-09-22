43b838ec211014e0c1c3c3875692d42750c712b0
Edrysian <harrisre19@gmail.com>
2024-09-22 17:33:09 +0100

    Revert "Adding database and about us page"
    This reverts commit f1650b7e6c1f91e488d325690e15fd83cb62bf6b.

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/about', [PageController::class, 'about'])->name('about');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
